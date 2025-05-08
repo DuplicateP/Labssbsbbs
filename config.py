15b833e7ac39e56c05210f3792bef6444d45ef73
DuplicateP <modassirjawaid44@gmail.com>
2025-05-08 13:16:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    API_ID = int(os.environ.get("20114039"))
-    API_HASH = os.environ.get("87297b8f3cc8fc9bbce591ad30da5896")
+    API_ID = int(os.environ.get("26792945"))
+    API_HASH = os.environ.get("6be4bc36c6a70c269f80a7388e59e150")
     VIP_USER = os.environ.get('VIP_USERS', '').split(',')
-    VIP_USERS = [int(8172163893) for user_id in VIP_USER]
+    VIP_USERS = [int(7357284659) for user_id in VIP_USER]
